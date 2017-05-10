1da5727637b590d960e9aac7d992f844bf0df758
Kyle Aardal <aardal.kyle@gmail.com>
2017-05-10 14:44:12 -0700

    blank article select now repopulates all articles

--- starter-code/scripts/articleView.js
+++ starter-code/scripts/articleView.js
@@ -37,21 +37,12 @@ articleView.handleAuthorFilter = function() {
     //         defining. "$(this)" is using jQuery to select that element, so we can chain jQuery methods
     //         onto it.
     if ($(this).val()) {
-      // TODO: If the select box was changed to an option that has a value, we need to hide all the articles,
-      //       and then show just the ones that match for the author that was selected.
-      //       Use an "attribute selector" to find those articles, and fade them in for the reader.
-      // $('select[name="icecream"]').on('change', function() {
-      //   var $selection = $(this).val();
-      //   $('img').hide()
-      //   $('img[data-flavor="' + $selection + '"]').show()
-      // })
       var $selection = $(this).val();
       $('article').hide();
       $('[data-author="'+$selection+'"]').fadeIn(750);
     } else {
-      // TODO: If the select box was changed to an option that is blank, we should
-      //       show all the articles, except the one article we are using as a template.
-
+      $('article').fadeIn(750);
+      $('article.template').hide();
     }
     $('#category-filter').val('');
   });
